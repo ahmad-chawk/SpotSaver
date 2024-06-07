3faadb08a0b87f1c278d8971833736baa0054e14
Ahmad Chawk <ahmad.m.chawk@gmail.com>
2024-06-07 20:55:35 +0300

    commit before pushing

---
+++ README.md
@@ -0,0 +1,10 @@
+# SpotSaver
+
+<img src= "screenshots/mainPage.png" width="200">   <img src= "screenshots/addItem.png" width="200">   <img src= "screenshots/locationExample.png" width="200">   <img src= "screenshots/locationsList.png" width="200">
+
+SpotSaver is a flutter multiplatform application used to save your favorites places. The app
+gives the user the ability save locations. Each loaction item consists of:
+
+- Title
+- Image
+- Location
---
+++ lib/screens/place_detail.dart
@@ -0,0 +1,79 @@
+import 'package:SpotSaver/models/palce.dart';
+import 'package:SpotSaver/screens/map.dart';
+import 'package:flutter/material.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({super.key, required this.place});
+  final Place place;
+
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lon = place.location.longitude;
+    return 'add your google maps api here';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => MapScreen(
+                          location: place.location,
+                          isSelecting: false,
+                        ),
+                      ),
+                    );
+                  },
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
